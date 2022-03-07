ed204908577995d77d87153915782f3c4569f53f
Ismet Dailov <60391985+ismetdailov@users.noreply.github.com>
2022-03-07 20:10:50 +0200

    working on profile view

---
+++ Web/PetsFriends.Web/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetsFriends.Web.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult MyProfile()
+        {
+            return View();
+        }
+    }
+}
