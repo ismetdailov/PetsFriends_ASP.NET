be19e1d40bc1295d380d967be35e38d36c2e7b25
Ismet Dailov <60391985+ismetdailov@users.noreply.github.com>
2022-03-13 19:38:22 +0200

    working on message view

---
+++ Web/PetsFriends.Web/Controllers/MessagesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetsFriends.Web.Controllers
+{
+    public class MessagesController : Controller
+    {
+        public IActionResult Messages()
+        {
+            return View();
+        }
+    }
+}
